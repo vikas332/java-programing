178f0dfac43260d893767c0022287e76980c9f26
vikas kumawat <73782963+vikas332@users.noreply.github.com>
2021-12-09 21:02:23 +0530

    Add files via upload

---
+++ 1_scannerclass/1_sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class sum1{
+public static void main(String[] arg)
+{       
+               Scanner input = new Scanner(System.in);
+	       System.out.println("enter 1st value: ");
+	       int a = input.nextInt();
+               System.out.println("enter 2nd value: ");
+               int b = input.nextInt();
+               int c= a+b;
+	       System.out.println("sum :" +c);
+}}
---
+++ 1_scannerclass/2_studentdata.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class studentdata{
+
+		    public static void main(String[] args){
+                     Scanner input = new Scanner(System.in);
+                     System.out.println("enter sub1 num: ");
+                      int a = input.nextInt();
+                    System.out.println("enter sub2 num: ");
+                      int b = input.nextInt();
+                     System.out.println("enter sub3 num: ");
+                      int c = input.nextInt();
+                      System.out.println("enter sub4 num: ");
+                      int d = input.nextInt();
+                    System.out.println("enter sub5 num: ");
+                      int e = input.nextInt();
+                     int t = a+b+c+d+e;
+                    System.out.println("total num : "+t);
+                    int p =t/5;
+		    System.out.println("per: "+p);
+
+
+                  
+}
+	
+}
