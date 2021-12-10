b271804a912417947deb895e59bb57822d222bca
vikas kumawat <73782963+vikas332@users.noreply.github.com>
2021-12-10 18:25:09 +0530

    Add files via upload

---
+++ 2_if_else/oddeven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class oddeven{
+               public static void main(String[] args)
+		{ 
+               Scanner input = new Scanner(System.in);
+	       System.out.println("enter value: ");
+	       int a = input.nextInt();
+			if(a%2==0)
+			{System.out.println("even");}
+			else
+			{System.out.println("odd");}
+}
+}
