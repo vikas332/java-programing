c4bc755cf0c7e10d64202987d20dc4472e4dab54
vikas kumawat <73782963+vikas332@users.noreply.github.com>
2021-12-10 18:14:33 +0530

    Delete 4_leapyear.java

--- 4_leapyear.java
+++
@@ -1,6 +0,0 @@
-class leapyear{
-           public static void main(String[] args)
-{	int y=2020;
-if(y%4==0){ 	System.out.println("leap year");}
-else{System.out.println("not leap year");}							
-}}n
